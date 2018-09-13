5af95523a2d6d50efc2cd1c8313c75c801cbf742
Shi Hao Hong <shihaohong94@gmail.com>
2018-09-13 15:44:04 -0700

    added successful render test to ReviewEntryHeader

--- client/components/__tests__/ReviewEntryHeader.test.js
+++ client/components/__tests__/ReviewEntryHeader.test.js
@@ -5,5 +5,15 @@ import ReviewEntryHeader from '../ReviewEntryHeader';
 import sampleReviews from './data/sampleReviews';
 
 describe('ReviewEntryHeader test suite', () => {
+  const defaultProps = {
+    'date': '2018-05-19T01:16:04.000Z',
+    'userImage': 'https://s3.us-east-2.amazonaws.com/fakebnb-reviews/6.jpg',
+    'username': 'Blake Mann Sr.'
+  };
 
+  it('renders without crashing', () => {
+    const container = document.createElement('div');
+    const reviews = [];
+    ReactDOM.render(<ReviewEntryHeader {...defaultProps} />, container);
+  });
 });
